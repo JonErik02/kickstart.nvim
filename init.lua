2684def5b2ecaf4face49ebc878d05a84fc7c37c
JonErik02 <jonathan.eriksson0@gmail.com>
2025-08-23 16:31:11 +0200

    added shortcut for neo-tree

--- init.lua
+++ init.lua
@@ -177,7 +177,7 @@ vim.o.tabstop = 4
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
+vim.keymap.set('n', '<leader>a', '<Cmd>Neotree toggle<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
